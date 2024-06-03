b002e92056248f73ee2d0f6af895613ffb89e78e
taxuanPhuoc <phuocta49@gmail.com>
2024-06-03 09:28:05 +0700

    Merge branch 'master' of https://github.com/27102002thang/SDLC

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,23 +7,26 @@ use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function showdata(Request $request){
+    public function showdata(Request $request)
+    {
         $keyword = $request->input('keyword');
         if ($keyword) {
             $keyword = '%' . trim($keyword) . '%';
             $listProduct = DB::table('cars')
                 ->where('name', 'like', $keyword)
-                ->               paginate(8);
+                ->paginate(8);
             $totalResults = DB::table('cars')
                 ->where('name', 'like', $keyword)
                 ->count();
-        }else{
-            $listProduct =DB::table('cars')->paginate(8);
+        } else {
+            $listProduct = DB::table('cars')->paginate(8);
             $totalResults = DB::table('cars')->count();
         }
-        return view("/customer.content", compact('listProduct','totalResults'));
+        return view("/customer.content", compact('listProduct', 'totalResults'));
     }
-    public function detail($id){
+
+    public function detail($id)
+    {
         $detail = DB::table('cars')->where('id', $id)->first();
 
         return view("/customer.detail", [
@@ -36,6 +39,7 @@ class UserController extends Controller
     {
         return view('/customer.order', ['id' => $id]);
     }
+
     public function store(Request $request)
     {
         $request->validate([
@@ -43,8 +47,8 @@ class UserController extends Controller
             'email' => 'required|string|email|max:255',
             'phone' => 'required|string|max:20',
             'address' => 'required|string|max:255',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
+            'order_date' => 'required|date',
+            'return_date' => 'required|date|after_or_equal:start_date',
             'note' => 'required|string|max:255',
         ]);
 
@@ -56,13 +60,17 @@ class UserController extends Controller
             'phone' => $request->phone,
             'order_date' => $request->start_date,
             'return_date' => $request->end_date,
-            'address' => $request ->address,
+            'address' => $request->address,
             'note' => $request->note,
 //            'created_at' => now(),
 //            'updated_at' => now(),
         ]);
 
-        return redirect()->route('customer.content')->with('message', 'Thuê xe thành công!');
+        $script = "<scrip>
+        alert('Đã xác nhận thông tin của quý khác! Xin vui lòng đợi phản hồi từ chúng tôi!');
+        </scrip>";
+
+        return $script . redirect()->route('customer.content')->with('message', 'Thuê xe thành công!');
     }
 
 
